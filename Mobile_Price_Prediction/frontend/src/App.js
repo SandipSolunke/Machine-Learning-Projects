4c530d1f60c23dcdfbc5c33432cb23b2a3685124
Sandip Solunke <89003941+SandipSolunke@users.noreply.github.com>
2022-09-30 22:00:27 +0530

    Update App.js

--- Mobile_Price_Prediction/frontend/src/App.js
+++ Mobile_Price_Prediction/frontend/src/App.js
@@ -45,93 +45,86 @@ function App() {
   return (
     <div className="App">
 
-      <h1 style={{ fontFamily: "monospace", fontSize: "35px" , color:"white"}}>Predict Mobile Price</h1>
+      <div className='heading'>
+        <h1 >Predict Mobile Price</h1>
+      </div>
 
-      <div style={{ backgroundColor: "#303030", marginLeft: "20%", marginRight: "20%", marginTop: "5px", boxShadow: " 0 4px 8px 0 #585858, 0 6px 15px 5px #585858"}}>
+      <div className="box">
 
-
-        <div style={{ position: "relative", margin: "0 auto", height: "auto" }}>
-          <form onSubmit={handleSubmit}>
+        <form className="form" onSubmit={handleSubmit} >
+          <br></br>
+          <p>
+            <label  >Enter RAM (GB):</label>
             <br></br>
+            <input type="text" onChange={event => setRAM(event.target.value)} value={RAM} />
+          </p>
+          <br></br>
 
-            <div style={{ justifyContent: "center" }} >
-              <label style={{ fontSize: "15px", fontFamily: "initial", justifyContent: "left", color:"white" }}>Enter RAM (GB):</label>
-              <br></br>
-              <input type="text" onChange={event => setRAM(event.target.value)} value={RAM} />
-            </div>
-
+          <p>
+            <label >Enter ROM (GB):</label>
             <br></br>
+            <input type="text" value={ROM} onChange={event => setROM(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px", color:"white" }}>Enter ROM (GB):</label>
-              <br></br>
-              <input type="text" value={ROM} onChange={event => setROM(event.target.value)} />
-            </p>
+          <br></br>
 
+          <p>
+            <label >Enter Display Size (inch):</label>
             <br></br>
+            <input type="text" value={Display} onChange={event => setDisplay(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px" , color:"white"}}>Enter Display Size (inches):</label>
-              <br></br>
-              <input type="text" value={Display} onChange={event => setDisplay(event.target.value)} />
-            </p>
+          <br></br>
 
+          <p>
+            <label >Enter Battery (MaH):</label>
             <br></br>
+            <input type="text" value={Battery} onChange={event => setBattery(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px" , color:"white"}}>Enter Battery (MaH):</label>
-              <br></br>
-              <input type="text" value={Battery} onChange={event => setBattery(event.target.value)} />
-            </p>
+          <br></br>
 
+          <p>
+            <label >Enter Selfi Cam Resolution(MP):</label>
             <br></br>
+            <input type="text" value={S_Cam} onChange={event => setS_Cam(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px", color:"white" }}>Enter Selfi Cam Resolution(MP):</label>
-              <br></br>
-              <input type="text" value={S_Cam} onChange={event => setS_Cam(event.target.value)} />
-            </p>
+          <br></br>
 
+          <p>
+            <label >Enter Primary Cam Resolution(MP) :</label>
             <br></br>
+            <input type="text" value={P_Cam} onChange={event => setP_Cam(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px" , color:"white"}}>Enter Primary Cam Resolution(MP) :</label>
-              <br></br>
-              <input type="text" value={P_Cam} onChange={event => setP_Cam(event.target.value)} />
-            </p>
+          <br></br>
 
+          <p>
+            <label >Enter Rating (0-5):</label>
             <br></br>
+            <input type="text" value={Rating} onChange={event => setRating(event.target.value)} />
+          </p>
 
-            <p>
-              <label style={{ fontSize: "15px", fontFamily: "initial", marginLeft: "0px" , color:"white"}}>Enter Rating (0-5):</label>
-              <br></br>
-              <input type="text" value={Rating} onChange={event => setRating(event.target.value)} />
-            </p>
-
-            <br></br>
+          <br></br>
 
 
-            <p>
-              <button type="submit" style={{ fontSize: "25px", fontFamily: "initial", marginLeft: "0px" }}>Submit</button>
-            </p>
+          <p>
+            <button type="submit" style={{ fontSize: "25px", fontFamily: "initial", marginBottom: "20px" }}>Submit</button>
+          </p>
 
-          </form>
-        </div>
-
-        <br></br>
-        
-        <br></br>
-        <br></br>
-
+        </form>
 
       </div>
 
-      <div style={{ fontSize: "30px", fontWeight: "bolder", fontFamily: "initial", marginLeft: "0px" ,color:"white", marginTop:"40px" , marginBottom:"10px"}}>
-           Predicted Price : {Price}
-        </div>
+      <div className='result'>
+        Predicted Price : {Price}
+      </div>
 
       <br></br>
+
     </div>
+
   );
 }
 
